1f83237421f2f472e811ec869becdb090bf31726
Wesley Lima <wesleyguedes7@gmail.com>
2024-03-16 18:22:56 -0300

    Stark Seguidores project finished

---
+++ src/screens/WebView/styles.js
@@ -0,0 +1,13 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  loadingContainer: {
+    alignItems: "center",
+    backgroundColor: "#6431F8",
+    height: "100%",
+    justifyContent: "center",
+  },
+});
